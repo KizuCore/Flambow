cab720ec3d815666ad757da19ae24487b341688e
Theo22100 <theoguerinBZH@hotmail.com>
2024-12-18 10:58:25 +0100

    refactor: update CSS imports to use alias for styles directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Équivalent de __dirname pour les modules ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@style': path.resolve(__dirname, 'src/assets/style'), // Alias pour le dossier style
+    },
+  },
+});
