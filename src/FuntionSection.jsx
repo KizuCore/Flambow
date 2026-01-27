75bb1b5c7b2f28a267b44e083d2e6dd9a8596ecf
camillebraun <camille.clavier.braun@gmail.com>
2026-01-27 22:09:08 +0100

    feature: rebranding flambow

--- src/FuntionSection.jsx
+++ src/FuntionSection.jsx
@@ -5,125 +5,129 @@ import twoHeads from "@image/two-heads.svg";
 function FunctionIntro() {
   return (
     <section className="py-5 bg-dark-blue text-white">
-      <div className="container mb-4">
-        {/* Titre + texte */}
-        <div className="text-center mb-5">
-          <h2 className="fw-bold title uppercase text-white mb-5 mt-5">
-            Comment ça fonctionne ?
-          </h2>
-          <p className="text-one pt-4">
-            <span className="fw-bold text-title">
-              Faites le lien : les seniors enseignent aux jeunes, et vice versa !
-            </span>
-          </p>
-          <p className="text-title">
-            Échangez des compétences auprès de volontaires motivés.
-          </p>
-        </div>
-
-        {/* Grille contenu */}
-        <div className="row fw-bold py-5">
-          {/* Liste des étapes */}
-          <div className="col-md-8">
-            <h3 className="fw-bold mb-5 text-title">
-              Tu souhaites apprendre :
-            </h3>
-            <ul className="list-unstyled">
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">1</span>
-                <p className="mb-0 text-li">Crée ton compte</p>
-              </li>
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">2</span>
-                <p className="mb-0 text-li">
-                  Renseigne les compétences que tu voudrais acquérir sur Flambow
-                </p>
-              </li>
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">3</span>
-                <p className="mb-0 fw-bold text-li">
-                  Si une annonce t&#39;intéresse, trouve une disponibilité avec
-                  la personne qui la propose
-                </p>
-              </li>
-              <li className="d-flex">
-                <span className="step-circle me-5">4</span>
-                <p className="mb-0 text-li">
-                  Apprends une nouvelle compétence !
-                </p>
-              </li>
-            </ul>
+      <div className="functionContainer">
+        <div className="container mb-4">
+          {/* Titre + texte */}
+          <div className="text-center mb-5">
+            <h2 className="fw-bold title uppercase text-white mb-5 mt-5">
+              Comment ça fonctionne ?
+            </h2>
+            <p className="text-one pt-4">
+              <h3 className="fw-bold text-title">
+                Faites le lien : les seniors enseignent aux jeunes, et vice
+                versa !
+              </h3>
+            </p>
+            <h4 className="text-title">
+              Échangez des compétences auprès de volontaires motivés.
+            </h4>
           </div>
 
-          {/* SVG */}
-          <div className="col-md-4 d-flex justify-content-end align-items-center">
-            <img
-              src={headIdea}
-              alt="Personne ayant une idée"
-              className="mb-5"
-              style={{ height: "314px" }}
-            />
-          </div>
-        </div>
+          {/* Grille contenu */}
+          <div className="row  py-5">
+            {/* Liste des étapes */}
+            <div className="col-md-8">
+              <h3 className="fw-bold mb-5 text-title">
+                Tu souhaites apprendre :
+              </h3>
+              <ul className="list-unstyled">
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">1</span>
+                  <p className="mb-0 text-li">Crée ton compte</p>
+                </li>
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">2</span>
+                  <p className="mb-0 text-li">
+                    Renseigne les compétences que tu voudrais acquérir sur
+                    Flambow
+                  </p>
+                </li>
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">3</span>
+                  <p className="mb-0 fw-bold text-li">
+                    Si une annonce t&#39;intéresse, trouve une disponibilité
+                    avec la personne qui la propose
+                  </p>
+                </li>
+                <li className="d-flex justify-content-start">
+                  <span className="step-circle me-5">4</span>
+                  <p className="mb-0 text-li">
+                    Apprends une nouvelle compétence !
+                  </p>
+                </li>
+              </ul>
+            </div>
 
-        <div className="row fw-bold pt-5">
-          {/* SVG */}
-          <div className="col-md-5 d-flex justify-content-start align-items-center">
-            <img
-              src={twoHeads}
-              alt="2 Personnes qui échangent leurs idées"
-              className="mb-5"
-              style={{ height: "290px" }}
-            />
+            {/* SVG */}
+            <div className="col-md-4 d-flex justify-content-end align-items-center">
+              <img
+                src={headIdea}
+                alt="Personne ayant une idée"
+                className="mb-5"
+                style={{ height: "314px" }}
+              />
+            </div>
           </div>
-          {/* Liste des étapes */}
-          <div className="col-md-7">
-            <h3 className="text-title fw-bold mb-5">
-              Tu souhaites partager tes compétences :
-            </h3>
-            <ul className="list-unstyled">
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">1</span>
-                <p className="mb-0 text-li">Crée ton compte</p>
-              </li>
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">2</span>
-                <p className="mb-0 text-li">
-                  Flambow, propose les compétences que tu veux partager
-                </p>
-              </li>
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">3</span>
-                <p className="mb-0 text-li">
-                  Tu reçois une notification si une personne est intéressée
-                </p>
-              </li>
-              <li className="d-flex mb-5">
-                <span className="step-circle me-5">4</span>
-                <p className="mb-0 text-li">
-                  Trouve une disponibilité avec cette personne
-                </p>
-              </li>
-              <li className="d-flex">
-                <span className="step-circle me-5">5</span>
-                <p className="mb-0 text-li">Partage-lui ta compétence !</p>
-              </li>
-            </ul>
+
+          <div className="row pt-5">
+            {/* SVG */}
+            <div className="col-md-5 d-flex justify-content-start align-items-center">
+              <img
+                src={twoHeads}
+                alt="2 Personnes qui échangent leurs idées"
+                className="mb-5"
+                style={{ height: "290px" }}
+              />
+            </div>
+            {/* Liste des étapes */}
+            <div className="col-md-7">
+              <h3 className="text-title fw-bold mb-5">
+                Tu souhaites partager tes compétences :
+              </h3>
+              <ul className="list-unstyled">
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">1</span>
+                  <p className="mb-0 text-li">Crée ton compte</p>
+                </li>
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">2</span>
+                  <p className="mb-0 text-li">
+                    Flambow, propose les compétences que tu veux partager
+                  </p>
+                </li>
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">3</span>
+                  <p className="mb-0 text-li">
+                    Tu reçois une notification si une personne est intéressée
+                  </p>
+                </li>
+                <li className="d-flex justify-content-start mb-5">
+                  <span className="step-circle me-5">4</span>
+                  <p className="mb-0 text-li">
+                    Trouve une disponibilité avec cette personne
+                  </p>
+                </li>
+                <li className="d-flex justify-content-start">
+                  <span className="step-circle me-5">5</span>
+                  <p className="mb-0 text-li">Partage-lui ta compétence !</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="video-container text-center my-5">
-        <h3 className="text-title fw-bold mb-5">
-          Découvrez la vidéo de démonstration !
-        </h3>
-        <div className="d-flex justify-content-center">
-          <div className="ratio ratio-16x9 w-75 w-md-50">
-            <iframe
-              src="https://www.youtube.com/embed/rDQa5mwbwWQ"
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+        <div className="video-container text-center my-5">
+          <h3 className="text-title fw-bold mb-5">
+            Découvrez la vidéo de démonstration !
+          </h3>
+          <div className="d-flex justify-content-center">
+            <div className="ratio ratio-16x9 w-75 w-md-50">
+              <iframe
+                src="https://www.youtube.com/embed/rDQa5mwbwWQ"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
--- src/NewsletteSection.jsx
+++ src/NewsletteSection.jsx
@@ -48,7 +48,7 @@ function NewsletterSection() {
             "X-Master-Key":
               "$2a$10$Zu66Zb596EucIxusOyr2g./I7qeHJAJY7ByzT3hNEWnrNQqW.2HV2",
           },
-        }
+        },
       );
 
       if (putResponse.status === 200) {
@@ -67,59 +67,61 @@ function NewsletterSection() {
 
   return (
     <section className="py-5 bg-dark-blue text-white">
-      <div className="container text-center mt-5">
-        {/* Titre */}
-        <h2 className="fw-bold mb-5 title">
-          Envie de rejoindre l’aventure ...
-        </h2>
-        <p className="mb-4 text-news fw-bold pb-4">
-          Suivez les dernières actualités en vous inscrivant à la newsletter !
-        </p>
+      <div className="newsLettercontainer">
+        <div className="container text-center mt-5">
+          {/* Titre */}
+          <h2 className="fw-bold mb-5 title">
+            Envie de rejoindre l’aventure ...
+          </h2>
+          <p className="mb-4 text-news fw-bold pb-4">
+            Suivez les dernières actualités en vous inscrivant à la newsletter !
+          </p>
 
-        {/* Formulaire news */}
-        <div className="d-flex justify-content-center pb-4">
-          <div className="input-group" style={{ maxWidth: "650px" }}>
-            {/* Champ de saisie */}
-            <input
-              type="email"
-              className="form-control email-input rounded-pill-start py-3 fw-bold"
-              placeholder="Votre adresse mail"
-              aria-label="Votre adresse mail"
-              value={email}
-              onChange={handleInputChange}
-              disabled={isLoading} // Désactiver champ en cours de traitement
-            />
-            {/* Bouton flèche */}
-            <button
-              className="arrow-btn rounded-pill-end"
-              type="button"
-              aria-label="Envoyer"
-              onClick={submitEmail}
-              disabled={isLoading} // Désactiver bouton en cours de traitement
-            >
-              {isLoading ? (
-                <div className="spinner-border custom-spinner" role="status">
-                  <span className="visually-hidden">Chargement...</span>
-                </div>
-              ) : (
-                <svg
-                  className="arrow-icon"
-                  width="50"
-                  height="30"
-                  viewBox="0 0 38 37"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    className="arrow-path"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M0.0874963 7.40215C-0.45417 2.53965 4.55208 -1.03118 8.975 1.06465L33.8583 12.8521C38.625 15.1084 38.625 21.8917 33.8583 24.148L8.975 35.9376C4.55208 38.0334 -0.452087 34.4626 0.0874963 29.6001L1.0875 20.5834H18C18.5525 20.5834 19.0824 20.3639 19.4731 19.9732C19.8638 19.5825 20.0833 19.0526 20.0833 18.5001C20.0833 17.9475 19.8638 17.4176 19.4731 17.0269C19.0824 16.6362 18.5525 16.4167 18 16.4167H1.08958L0.0874963 7.40215Z"
-                    fill="#1B1725"
-                  />
-                </svg>
-              )}
-            </button>
+          {/* Formulaire news */}
+          <div className="d-flex justify-content-center pb-4">
+            <div className="input-group" style={{ maxWidth: "650px" }}>
+              {/* Champ de saisie */}
+              <input
+                type="email"
+                className="form-control email-input rounded-pill-start py-3 fw-bold"
+                placeholder="Votre adresse mail"
+                aria-label="Votre adresse mail"
+                value={email}
+                onChange={handleInputChange}
+                disabled={isLoading} // Désactiver champ en cours de traitement
+              />
+              {/* Bouton flèche */}
+              <button
+                className="arrow-btn rounded-pill-end"
+                type="button"
+                aria-label="Envoyer"
+                onClick={submitEmail}
+                disabled={isLoading} // Désactiver bouton en cours de traitement
+              >
+                {isLoading ? (
+                  <div className="spinner-border custom-spinner" role="status">
+                    <span className="visually-hidden">Chargement...</span>
+                  </div>
+                ) : (
+                  <svg
+                    className="arrow-icon"
+                    width="50"
+                    height="30"
+                    viewBox="0 0 38 37"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      className="arrow-path"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M0.0874963 7.40215C-0.45417 2.53965 4.55208 -1.03118 8.975 1.06465L33.8583 12.8521C38.625 15.1084 38.625 21.8917 33.8583 24.148L8.975 35.9376C4.55208 38.0334 -0.452087 34.4626 0.0874963 29.6001L1.0875 20.5834H18C18.5525 20.5834 19.0824 20.3639 19.4731 19.9732C19.8638 19.5825 20.0833 19.0526 20.0833 18.5001C20.0833 17.9475 19.8638 17.4176 19.4731 17.0269C19.0824 16.6362 18.5525 16.4167 18 16.4167H1.08958L0.0874963 7.40215Z"
+                      fill="#1B1725"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </div>
